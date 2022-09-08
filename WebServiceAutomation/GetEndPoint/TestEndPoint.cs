13e408849510a8f9f71b5e6bbacf86483285d0e5
Archie Fan <archiefan@gmail.com>
2022-09-08 18:25:26 +0100

    Added more test cases

--- WebServiceAutomation/GetEndPoint/TestEndPoint.cs
+++ WebServiceAutomation/GetEndPoint/TestEndPoint.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using WebServiceAutomation.Models;
 using Xunit.Abstractions;
 using static System.Net.WebRequestMethods;
 
@@ -170,5 +171,65 @@ namespace WebServiceAutomation.GetEndPoint
             // Close the connection
             client.Dispose();
         }
+
+        [Fact]
+        public void TestEndPointUsingSendAsync()
+        {
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
+            httpRequestMessage.RequestUri = new Uri(geturl);
+            httpRequestMessage.Method = HttpMethod.Get;
+            httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpClient client = new HttpClient();
+            Task<HttpResponseMessage> httpResponse = client.SendAsync(httpRequestMessage);
+            HttpResponseMessage responseMessage = httpResponse.Result;
+            output.WriteLine(responseMessage.ToString());
+            HttpStatusCode statusCode = responseMessage.StatusCode;
+            output.WriteLine($"Status Code =>  {statusCode.ToString()}");
+            output.WriteLine($"Status Code =>  {(int)statusCode}");
+
+            // Response data
+            HttpContent responseContent = responseMessage.Content;
+            Task<string> responseData = responseContent.ReadAsStringAsync();
+            string data = responseData.Result;
+            output.WriteLine(data);
+
+            // Close the connection
+            client.Dispose();
+        }
+
+        [Fact]
+        public void TestUsingStatement()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, geturl))
+                {
+                    httpRequestMessage.RequestUri = new Uri(geturl);
+                    httpRequestMessage.Method = HttpMethod.Get;
+                    httpRequestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    Task<HttpResponseMessage> httpResponse = client.SendAsync(httpRequestMessage);
+                    
+                    using (HttpResponseMessage responseMessage = httpResponse.Result )
+                    {
+                        //output.WriteLine(responseMessage.ToString());
+                        HttpStatusCode statusCode = responseMessage.StatusCode;
+                        //output.WriteLine($"Status Code =>  {statusCode.ToString()}");
+                        //output.WriteLine($"Status Code =>  {(int)statusCode}");
+
+                        // Response data
+                        HttpContent responseContent = responseMessage.Content;
+                        Task<string> responseData = responseContent.ReadAsStringAsync();
+                        string data = responseData.Result;
+                        //output.WriteLine(data);
+
+                        RestResponse restResponse = new RestResponse((int)statusCode, responseData.Result);
+                        output.WriteLine(restResponse.ToString());
+                    }
+                }
+            }
+        }
+    
     }
 }
---
+++ WebServiceAutomation/Models/RestResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceAutomation.Models
+{
+    public class RestResponse
+    {
+        private readonly int _stateCode;
+        private readonly string _responseData;
+
+        public RestResponse(int stateCode, string responseData)
+        {
+            _stateCode = stateCode;
+            _responseData = responseData;
+        }
+
+        public int StateCode
+        {
+            get { return _stateCode; }
+        }
+
+        public string ResponseData { 
+            get { return _responseData; } 
+        }
+
+        public override string ToString()
+        {
+            return string.Format("StateCode: {0} ResponseData: {1} ", StateCode, ResponseData);
+        }
+    }
+}
